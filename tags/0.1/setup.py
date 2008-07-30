d83e2e7c2cd413df79a195e7e903aa82fe219f5f
iain.lowe <iain.lowe@5ff8ba9e-e719-0410-9a6b-a16cffc36142>
2008-07-30 01:04:26 +0000

    Tagging 0.1 code for release

---
+++ tags/0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+from byteplay import __version__ as lib_version
+
+setup(
+       name = 'byteplay',
+       version = lib_version,
+       py_modules = ['byteplay'],
+       zip_safe = True,
+)
