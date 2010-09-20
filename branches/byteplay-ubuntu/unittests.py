07c72de5ed7a49db5ab0637badccf25045162812
iain.lowe <iain.lowe@5ff8ba9e-e719-0410-9a6b-a16cffc36142>
2010-09-20 19:12:21 +0000

    Removed Ubuntu packaging branch

--- branches/byteplay-ubuntu/unittests.py
+++
@@ -1,6 +0,0 @@
-def test_python26_build_map():
-	import sys
-	
-	if '.'.join(str(x) for x in sys.version_info[:2]) == '2.6':
-		from byteplay import Code
-		Code.from_code((lambda: {'a': 1}).func_code).to_code()
